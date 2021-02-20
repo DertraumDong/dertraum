d3d7fa588137eef94562ec944a5f5dae9378115a
liudong <liudong220@126.com>
2021-02-20 16:10:01 +0800

    数据库脚本更新

--- src/main/resources/sql/relation.sql
+++ src/main/resources/sql/relation.sql
@@ -2,7 +2,7 @@ DROP table if exists human_relation;
 CREATE TABLE if not exists human_relation (
 	user_id varchar(32) NOT NULL COMMENT '用户id',
 	human_id varchar(32) NOT NULL COMMENT '人员id',
-	creation_date date NOT NULL COMMENT '关系创建时间'
+	creation_date datetime NOT NULL COMMENT '关系创建时间'
 ) COMMENT='人员关系表';
 
 DROP table if exists human_info;
@@ -12,9 +12,9 @@ CREATE TABLE if not exists human_info (
 	birthday date COMMENT '新历生日',
 	old_birthday date COMMENT '农历生日',
 	creation_by varchar(32) NOT NULL COMMENT '创建者',
-	creation_date date NOT NULL COMMENT '创建时间',
+	creation_date datetime NOT NULL COMMENT '创建时间',
 	modify_by varchar(32) NOT NULL COMMENT '修改者',
-	modify_date date NOT NULL COMMENT '修改时间',
+	modify_date datetime NOT NULL COMMENT '修改时间',
 	version int NOT NULL COMMENT '版本号',
 	PRIMARY KEY (human_id)
 ) COMMENT='人员表';
